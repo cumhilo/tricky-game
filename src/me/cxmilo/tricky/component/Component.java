a86439ea258b6641cd4fdfac1d6a4b3bc80dc610
cxmil <zdevelopxr@gmail.com>
2022-07-30 12:06:09 -0500

    feat: add component system

---
+++ src/me/cxmilo/tricky/component/Component.java
@@ -0,0 +1,14 @@
+package me.cxmilo.tricky.component;
+
+public interface Component {
+
+    /**
+     * Render the component.
+     */
+    void render();
+
+    /**
+     * Update the component.
+     */
+    void update();
+}
